88a0b4dd3374a9eab6679093b582cb9d9ae8f3d8
NagaiMatsuge <komilovboburweb@gmail.com>
2025-01-21 18:49:25 +0500

    feat: Added extra roles for jwt validation

--- auth/main.go
+++ auth/main.go
@@ -84,7 +84,7 @@ func validateJWT(next http.Handler) http.Handler {
 
 		// Extract claims and validate role
 		claims, ok := token.Claims.(*MyCustomClaims)
-		if !ok || !In(claims.Role, "ADMIN", "USER", "COURIER", "MANAGER", "CLIENT", "VENDOR", "INTERNAL") {
+		if !ok || !In(claims.Role, "ADMIN", "USER", "COURIER", "MANAGER", "CLIENT", "VENDOR", "INTERNAL", "VENDOR_ADMIN", "VENDOR_OPERATOR", "VENDOR_OBSERVER") {
 			http.Error(w, "Forbidden", http.StatusForbidden)
 			return
 		}
